617d4c211082764cf6862a153656352cc3685846
nhhandevops <148748303+nhhandevops@users.noreply.github.com>
2024-04-30 10:11:12 +0700

    Update score.py
    get model from blob store

--- test/score.py
+++ test/score.py
@@ -4,12 +4,27 @@
 from azureml.core.model import Model
 import joblib
 import pandas as pd
+import sys
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, generate_blob_sas, BlobSasPermissions
 
+account_name = "mloptestsa"
+container_name = "mloptestcontainer"
+directory_name = "model_output"
+model_name = "price_car_data.pkl"
+blob_name = f"{directory_name}/{model_name}"
 
+# Load the model from Blob Storage
+def load_model_from_blob_storage(connection_string):
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    with open(model_name, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
+    return joblib.load(model_name)
 
-def init():
-    model_path = Model.get_model_path(model_name="price_car_data.pkl")
-    model = joblib.load(model_path)
+def init(connection_string):
+    # model_path = Model.get_model_path(model_name="price_car_data.pkl")
+    # model = joblib.load(model_path)
+    model = load_model_from_blob_storage(connection_string)
     return model
 
 def predict(model):
@@ -18,8 +33,10 @@ def predict(model):
     print("Predicted selling price for the 7car: ", predicted_price)
 
 def main():
-
-    model = init()
+    arguments = sys.argv
+    account_key = str(arguments[1])
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    model = init(connection_string)
     predict(model)
 
 if __name__ == '__main__':
